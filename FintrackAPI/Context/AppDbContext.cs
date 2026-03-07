a3b157fe0c9c9b58e87e5c439a4c1daa67009814
lucasborgss1 <lucasaborgesdev1@gmail.com>
2026-03-07 20:47:49 -0300

    Update project configuration and database context
    - Added `appsettings.json` to `.gitignore`.
    - Introduced `AppDbContext` class in `AppDbContext.cs` with `DbSet` properties for `Categoria`, `TipoTransacao`, and `Transacao`.
    - Updated `FintrackAPI.csproj` to include new package references for OpenAPI and MySQL support.
    - Modified `Program.cs` to configure `AppDbContext` with MySQL connection and removed controller service registration.
    - Removed logging configuration and allowed hosts settings from `appsettings.json`.

---
+++ FintrackAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FintrackAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FintrackAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options ) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<TipoTransacao> TipoTransacoes { get; set; }
+    public DbSet<Transacao> Transacoes { get; set; }
+}
